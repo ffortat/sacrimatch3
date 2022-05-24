7338843ec1ee7fd44f6314ad5737b6a6aa4ab58d
Florent Fortat <florent@fortat.fr>
2022-05-25 00:32:11 +0200

    Initialise gem with SO

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -7,14 +7,18 @@ namespace Sacrimatch3
         [SerializeField]
         private new SpriteRenderer renderer = null;
 
-        private void Awake()
+        private SOGem gemData = null;
+
+        public void Setup(SOGem gemData)
         {
-            // TODO visual initialization
+            this.gemData = gemData;
+            renderer.sprite = gemData.sprite;
         }
 
         public Sprite Sprite {
             get => renderer.sprite;
             set => renderer.sprite = value;
         }
+        public SOGem Type { get => gemData; }
     }
 }
