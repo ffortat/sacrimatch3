1d999b76e80c757466f8037b4840bd577795570b
Florent Fortat <florent@fortat.fr>
2022-05-27 15:27:34 +0200

    Spawn characters

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,9 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Sacrimatch3
 {
     public class CharacterController : MonoBehaviour
     {
-        
+        [SerializeField]
+        private Character characterPrefab = null;
+        [SerializeField]
+        private int partySize = 5;
+        [SerializeField]
+        private List<SOCharacter> characters = new List<SOCharacter>();
+
+        private List<Character> party = new List<Character>();
+
+        private void Start()
+        {
+            for (int i = 0; i < partySize; i += 1)
+            {
+                Character character = Instantiate(characterPrefab, new Vector3(-16, 8), Quaternion.identity, transform);
+                character.Setup(characters[Random.Range(0, characters.Count)]);
+                party.Add(character);
+            }
+        }
     }
 }
