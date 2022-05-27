b9f9e138fb75249a2d4cf88296862a60dafe2765
Florent Fortat <florent@fortat.fr>
2022-05-27 11:20:23 +0200

    Add main menu to project

---
+++ Assets/Scripts/GameLoader.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sacrimatch3
+{
+    public class GameLoader : MonoBehaviour
+    {
+        /* TODO
+         * Singleton
+         * Charger une partie
+         */
+    }
+}
