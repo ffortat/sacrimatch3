1e51f70cb2673a19f8ef414ef00f54494369401c
Florent Fortat <florent@fortat.fr>
2022-05-27 15:08:43 +0200

    Link game systems together

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Sacrimatch3
+{
+    public class CharacterController : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace Sacrimatch3
+{
+    public class GameController : MonoBehaviour
+    {
+        /* TODO
+         * Initiliser le groupe de personnages
+         * Présenter le groupe de personnages face à une porte
+         * Séquence de sacrifice
+         * Initialisation du match 3
+         * Mise en scène
+         * *phase de jeu*
+         * Déclenchement du passage de porte
+         * Déclenchement de séquence de sacrifice
+         * Déclenchement de séquence de défaite
+         * Déclenchement de séquence de victoire
+         */
+        [SerializeField]
+        private CharacterController characterController = null;
+        [SerializeField]
+        private Match3 match3 = null;
+
+        private void Awake()
+        {
+            
+        }
+
+        private void Start()
+        {
+
+        }
+    }
+}
