d85a2adf65d73b377782cfb23cee869b1074576a
Florent Fortat <florent@fortat.fr>
2022-05-30 00:40:33 +0200

    Update Main menu

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Sacrimatch3.UI
 {
     public class MainMenuUI : MonoBehaviour
     {
+        [SerializeField]
+        private Button exitButton = null;
+
         private GameLoader gameLoader = null;
 
         private void Start()
         {
             gameLoader = FindObjectOfType<GameLoader>();
+
+#if UNITY_WEBGL
+            exitButton.gameObject.SetActive(false);
+#endif
         }
 
         public void Play()
